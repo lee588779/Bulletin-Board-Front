507ec1f43eebdc9acca3c6485589a72c404fe5f7
jmlee119 <jmlee119@naver.com>
2025-01-17 15:33:01 +0900

    feat : 공지사항은 관리자만 가능

--- js/boardList.js
+++ js/boardList.js
@@ -81,39 +81,43 @@ const boardData = [
 ];
 function generatePostList() {
   const postListContainer = document.getElementById("postList");
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const boardType = urlParams.get("boardType");
-  // console.log(boardType);
+
   boardData.map((post) => {
     const postContent = document.createElement("div");
     postContent.classList.add("postContent");
 
     const urlParams = new URLSearchParams(window.location.search);
     const boardType = urlParams.get("boardType");
-    console.log(boardType);
+
     const boardTypeDis = document.getElementById("boardTypeName");
     if (boardType == "0") {
       boardTypeDis.textContent = "공지사항";
-    }
-    if (boardTypeDis == "1") {
+    } else if (boardType == "1") {
       boardTypeDis.textContent = "자유게시판";
     }
+
     // 게시글 링크를 감싸는 a 태그 생성
     const postLink = document.createElement("a");
     postLink.href = `./postDetail.html?id=${post.id}`;
-    postLink.classList.add("postLink"); // 스타일링을 위한 클래스 추가
+    postLink.classList.add("postLink");
 
     // postLink 안에 내용을 추가
     postLink.innerHTML = `
       <span class="postId">${post.id}</span>
       <span class="postTitle">${post.title}</span>
-      <span class="postWriter">${post.name}</span>
+      <span class="postWriter" id="postWriter-${post.id}">${post.name}</span>
       <span class="postTime">${new Date(post.time).toLocaleString()}</span>
     `;
 
-    // postContent에 postLink 추가
+    // postContent에 postLink 추가 후 DOM에 추가
     postContent.appendChild(postLink);
     postListContainer.appendChild(postContent);
+
+    // DOM에 추가된 후 postWriter 선택
+    const postWriter = document.getElementById(`postWriter-${post.id}`);
+    if (boardType == "0") {
+      postWriter.textContent = "관리자";
+    }
   });
 }
 
